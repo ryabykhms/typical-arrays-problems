cda291fd6731271ef26504fbf1d56276a7d88fa1
ryabykhms <ryabykhms@gmail.com>
2020-09-28 20:21:16 +0300

    feat: implement avg function

--- src/index.js
+++ src/index.js
@@ -22,6 +22,12 @@ exports.avg = function avg(array) {
     if (array === undefined || !Array.isArray(array) || array.length === 0) {
         return 0;
     }
-};
 
-// console.log(min([-38,12,-25,-29,-29,-18,18,1,3,7,-27,22,32,5,16,0,-22,23,-37,9]));
+    return parseFloat(
+        (
+            array.reduce((sum, curr) => {
+                return sum + curr;
+            }, 0) / array.length
+        ).toFixed(10)
+    ).toString();
+};
