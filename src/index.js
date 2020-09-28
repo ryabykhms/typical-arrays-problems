b3781bdcb7fa945a12324d4128123316f54da143
ryabykhms <ryabykhms@gmail.com>
2020-09-28 20:08:15 +0300

    feat: implement min function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (array === undefined || !Array.isArray(array) || array.length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    return array.reduce((acc, curr) => {
+        return curr < acc ? curr : acc;
+    }, array[0]);
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || !Array.isArray(array) || array.length === 0) {
+        return 0;
+    }
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || !Array.isArray(array) || array.length === 0) {
+        return 0;
+    }
+};
+
+// console.log(min([-38,12,-25,-29,-29,-18,18,1,3,7,-27,22,32,5,16,0,-22,23,-37,9]));
